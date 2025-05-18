3d075aff9ad5353f90a3c47dad72178438c3a4db
fidelisobioma <mbamfidelisobioma1@gmail.com>
2025-05-18 18:22:57 +0100

    Updated my home page to incoporate dark mode

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -27,17 +27,17 @@ function Home() {
   };
 
   return (
-    <div className="p-12 bg-lightbg max-w-[1400px] my-0 min-h-screen mx-auto">
+    <div className="p-12 bg-lightbg dark:bg-darkbg950 max-w-[1400px] my-0 min-h-screen mx-auto">
       <div className="md:flex justify-between">
-        <div className="shadow bg-white relative grid items-center px-6 h-fit">
+        <div className="shadow bg-lightbg dark:bg-darkbg900 relative grid items-center px-6 h-fit">
           <Search
-            className="size-4 text-gray-500 absolute left-2 cursor-pointer"
+            className="size-4 text-lighttext dark:text-darktext absolute left-2 cursor-pointer"
             onClick={handleSearch}
           />
           <input
             type="text"
             placeholder="Search for a country..."
-            className="p-3 outline-none text-[hsl(0,0%,50%)] text-sm"
+            className="p-3 outline-none bg-lightbg dark:bg-darkbg900 text-[hsl(0,0%,50%)] dark:text-darktext text-sm"
             value={inputValue}
             onChange={handleInputValue}
           />
@@ -61,29 +61,37 @@ function Home() {
         </div>
       </div>
       {/*cards  */}
-      <div className="bg-lightbg md:grid md:grid-cols-2 lg:grid-cols-4 gap-8 m-auto mt-6">
+      <div className="bg-lightbg dark:bg-darkbg950 md:grid md:grid-cols-2 lg:grid-cols-4 gap-8 m-auto mt-6">
         {filteredCountry.map((data) => (
           <Link
             to={`/${data.name.common}`}
             key={data.name.common}
-            className=" grid justify-center shadow bg-lightbg rounded mt-12 md:mt-0 hover:scale-105 transition-transform duration-500 hover:cursor-pointer"
+            className=" grid justify-center shadow bg-lightbg dark:bg-darkbg900 rounded mt-12 md:mt-0 hover:scale-105 transition-transform duration-500 hover:cursor-pointer"
           >
             <div>
               <img src={data.flags.png} alt="" />
             </div>
             <div className="p-6 grid gap-2 text-sm">
-              <p className="font-bold text-sm text-lighttext">
+              <p className="font-bold text-sm text-lighttext dark:text-darktext">
                 {data.name.common}
               </p>
-              <p className="text-lighttext">
+              <p className="text-lighttext dark:text-darktext">
                 Population:
-                <span className="text-gray-500"> {data.population}</span>
+                <span className="text-gray-500 dark:text-gray-400">
+                  {data.population}
+                </span>
               </p>
-              <p className="text-lighttext">
-                Region: <span className="text-gray-500"> {data.region}</span>
+              <p className="text-lighttext dark:text-darktext">
+                Region:
+                <span className="text-gray-500 dark:text-gray-400 ">
+                  {data.region}
+                </span>
               </p>
-              <p className="text-lighttext">
-                Capital: <span className="text-gray-500"> {data.capital}</span>
+              <p className="text-lighttext dark:text-darktext">
+                Capital:
+                <span className="text-gray-500 dark:text-gray-400">
+                  {data.capital}
+                </span>
               </p>
             </div>
           </Link>
